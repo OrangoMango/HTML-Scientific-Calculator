6b2b9872b846a4a67dd4e1bd3294cfa491d81ab0
Orango Mango <61402409+OrangoMango@users.noreply.github.com>
2020-04-20 16:19:10 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,82 @@
+var selection = "rad"
+     
+function insert(num){
+    var text = document.form.textinput.value
+    document.form.textinput.value = text + num
+}
+
+function clean(){
+    document.form.textinput.value = ""
+}
+     
+function calculate(){
+    var text = document.form.textinput.value
+    document.form.textinput.value = eval(text)
+}
+     
+function back(){
+     var text = document.form.textinput.value
+     document.form.textinput.value = text.substring(0,text.length-1)
+}
+     
+function calc_sin(){
+     var text = document.form.textinput.value
+     if (selection == "rad"){
+       document.form.textinput.value = Math.sin(text)
+     } else if (selection == "deg"){
+       document.form.textinput.value = Math.sin(text * (Math.PI / 180))
+     }
+   }
+     
+function calc_cos(){
+   var text = document.form.textinput.value
+   if (selection == "rad"){
+     document.form.textinput.value = Math.tan(text)
+   } else if (selection == "deg"){
+     document.form.textinput.value = Math.tan(text * (Math.PI / 180))
+   }
+ }
+     
+function calc_tan(){
+   var text = document.form.textinput.value
+   if (selection == "rad"){
+     document.form.textinput.value = Math.cos(text)
+   } else if (selection == "deg"){
+     document.form.textinput.value = Math.cos(text * (Math.PI / 180))
+   }
+ }
+ 
+ function calc_sqrt(){
+ var text = document.form.textinput.value
+ document.form.textinput.value = Math.sqrt(text)
+ }
+ 
+ function calc_percent(){
+ var text = document.form.textinput.value
+ document.form.textinput.value = text/100
+ }
+ 
+ function change_label_selection(){
+   var label = document.getElementById("info")
+   label.innerHTML = "<b>"+selection.toUpperCase()+"</b>"
+ }
+ 
+ function calc_deg(){
+   var element = document.getElementById("deg")
+   element.style = "border: solid red"
+   var element2 = document.getElementById("rad")
+   element2.style = "border solid"
+   
+   selection = "deg" //Set selection to deg
+   change_label_selection()
+ }
+ 
+ function calc_rad(){
+   var element2 = document.getElementById("rad")
+   element2.style = "border: solid red"
+   var element = document.getElementById("deg")
+   element.style = "border: solid"
+   
+   selection = "rad" //Set selection to rad
+   change_label_selection()
+ }
