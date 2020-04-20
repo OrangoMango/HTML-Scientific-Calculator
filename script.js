4815f344c38c4268abd04c823ded9c134df4a7ee
Orango Mango <61402409+OrangoMango@users.noreply.github.com>
2020-04-20 14:38:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -41,3 +41,15 @@ function calc_percent(){
       var text = document.form.textinput.value
       document.form.textinput.value = text/100
       }
+
+function calc_deg(){
+        var text = document.form.textinput.value
+        var out = text * (Math.PI / 180)
+        document.form.textinput.value = out
+      }
+      
+function calc_rad(){
+        var text = document.form.textinput.value
+        var out = (text / 180) * Math.PI
+        document.form.textinput.value = out
+      }
